23354f6744adb58532b3e2f5224b2882bba368bf
Harunur Rashid <guest.batworld@gmail.com>
2023-06-22 00:10:21 +0600

    unique slug package adding

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use App\Http\Controllers\NotificationController;
 use App\Http\Livewire\ChatGpt;
 use App\Jobs\ProcessPodcast;
+use App\Models\User;
+use HarunurRashid\LaravelUniqueSlug\Facades\UniqueSlug;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function(){
+    $slug =  UniqueSlug::generate(User::class, 'lala', 'lala@gmail.com');
+    dd($slug);
     ProcessPodcast::dispatch();
     return view('index');
 });
